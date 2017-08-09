b2e711968bafcd4b669b0707a5444ef94b41046f
Ac-heron <acheron20122012@gmail.com>
2017-08-09 19:25:47 +0800

    Refactor Param class

--- src/main/java/com/herohuang/framework/bean/Param.java
+++ src/main/java/com/herohuang/framework/bean/Param.java
@@ -2,7 +2,12 @@ package com.herohuang.framework.bean;
 
 import com.herohuang.framework.util.CastUtil;
 import com.herohuang.framework.util.CollectionUtil;
+import com.herohuang.framework.util.StringUtil;
 
+import java.net.FileNameMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,19 +19,55 @@ import java.util.Map;
  */
 public class Param {
 
-    private Map<String, Object> paramMap;
+    private List<FormParam> formParamList;
+    private List<FileParam> fileParamList;
 
-    public Param(Map<String, Object> paramMap) {
-        this.paramMap = paramMap;
+    public Param(List<FormParam> formParamList) {
+        this.formParamList = formParamList;
+    }
+
+    public Param(List<FormParam> formParamList, List<FileParam> fileParamList) {
+        this.formParamList = formParamList;
+        this.fileParamList = fileParamList;
     }
 
     /**
-     * 获取所有参数
-     *
-     * @return
+     * 获取请求参数映射
      */
-    public Map<String, Object> getMap() {
-        return paramMap;
+    public Map<String, Object> getFieldMap() {
+        Map<String, Object> fieldMap = new HashMap<>();
+        if (CollectionUtil.isNotEmpty(formParamList)) {
+            for (FormParam formParam : formParamList) {
+                String fieldName = formParam.getFieldName();
+                Object fieldValue = formParam.getFieldValue();
+                if (fieldMap.containsKey(fieldName)) {
+                    fieldValue = fieldMap.get(fieldName) + StringUtil.SEPARATOR + fieldValue;
+                }
+                fieldMap.put(fieldName, fieldValue);
+            }
+        }
+        return fieldMap;
+    }
+
+    /**
+     * 获取上传文件映射
+     */
+    public Map<String, List<FileParam>> getFileMap() {
+        Map<String, List<FileParam>> fileMap = new HashMap<>();
+        if (CollectionUtil.isNotEmpty(fileParamList)) {
+            for (FileParam fileParam : fileParamList) {
+                String fieldName = fileParam.getFieldName();
+                List<FileParam> fileParamList;
+                if (fileMap.containsKey(fieldName)) {
+                    fileParamList = fileMap.get(fieldName);
+                } else {
+                    fileParamList = new ArrayList<>();
+                }
+                fileParamList.add(fileParam);
+                fileMap.put(fieldName, fileParamList);
+            }
+        }
+        return fileMap;
     }
 
     /**

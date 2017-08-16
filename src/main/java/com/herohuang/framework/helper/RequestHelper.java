7a0607424ade21f61be56d9c4d33f842c41c799b
Acheron <acheron20122012@gmail.com>
2017-08-16 21:31:01 +0800

    Request hepler class

---
+++ src/main/java/com/herohuang/framework/helper/RequestHelper.java
@@ -0,0 +1,80 @@
+package com.herohuang.framework.helper;
+
+import com.herohuang.framework.bean.FormParam;
+import com.herohuang.framework.bean.Param;
+import com.herohuang.framework.util.ArrayUtil;
+import com.herohuang.framework.util.CodecUtil;
+import com.herohuang.framework.util.StreamUtil;
+import com.herohuang.framework.util.StringUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+/**
+ * 请求助手类
+ *
+ * @author Acheron
+ * @date 16/08/2017
+ * @since 1.0.0
+ */
+public final class RequestHelper {
+
+    /**
+     * 创建请求对象
+     */
+    public static Param createParam(HttpServletRequest request) throws IOException {
+        List<FormParam> formParamList = new ArrayList<>();
+        formParamList.addAll(parseParameterNames(request));
+        formParamList.addAll(parseInputStream(request));
+        return new Param(formParamList);
+    }
+
+    private static List<FormParam> parseParameterNames(HttpServletRequest request) {
+        List<FormParam> formParamList = new ArrayList<>();
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String fieldName = parameterNames.nextElement();
+            String[] fieldValues = request.getParameterValues(fieldName);
+            if (ArrayUtil.isNotEmpty(fieldValues)) {
+                Object fieldValue;
+                if (fieldValues.length == 1) {
+                    fieldValue = fieldValues[0];
+                } else {
+                    StringBuffer sb = new StringBuffer("");
+                    for (int i = 0; i < fieldValues.length; i++) {
+                        sb.append(fieldValues[i]);
+                        if (i != fieldValues.length - 1) {
+                            sb.append(StringUtil.SEPARATOR);
+                        }
+                    }
+                    fieldValue = sb.toString();
+                }
+                formParamList.add(new FormParam(fieldName, fieldValue));
+            }
+        }
+        return formParamList;
+    }
+
+
+    private static List<FormParam> parseInputStream(HttpServletRequest request) throws IOException {
+        List<FormParam> formParamList = new ArrayList<>();
+        String body = CodecUtil.decodeURL(StreamUtil.getString(request.getInputStream()));
+        if (StringUtil.isNotEmpty(body)) {
+            String[] kvs = StringUtil.splitString(body, "&");
+            if (ArrayUtil.isNotEmpty(kvs)) {
+                for (String kv : kvs) {
+                    String[] array = StringUtil.splitString(kv, "=");
+                    if (ArrayUtil.isNotEmpty(array) && array.length == 2) {
+                        String fieldName = array[0];
+                        String fieldValue = array[1];
+                        formParamList.add(new FormParam(fieldName, fieldValue));
+                    }
+                }
+            }
+        }
+        return formParamList;
+    }
+}

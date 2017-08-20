1c2a0af196fedbe0784e5dafffe6e28f4188f169
Acheron <acheron20122012@gmail.com>
2017-08-20 10:20:58 +0800

    Add common method of serlvetHelper

--- src/main/java/com/herohuang/framework/helper/SerlvetHelper.java
+++ src/main/java/com/herohuang/framework/helper/SerlvetHelper.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * servlet helper
@@ -74,11 +75,12 @@ public final class SerlvetHelper {
     }
 
 ///// 一些常用的方法：//////////////////////////////////////
+
     /**
      * 将属性放入request中
      */
     public static void setRequestAttribute(String key, Object value) {
-        getRequest().setAttribute(key,value);
+        getRequest().setAttribute(key, value);
     }
 
     /**
@@ -91,8 +93,49 @@ public final class SerlvetHelper {
     /**
      * 从request中移除属性
      */
-    public static void remoteRequestAttribute(String key) {
+    public static void removeRequestAttribute(String key) {
         getRequest().removeAttribute(key);
     }
+
+    /**
+     * 发送重定向响应
+     */
+    public static void sendRedirect(String location) {
+        try {
+            getResponse().sendRedirect(getRequest().getContextPath() + location);
+        } catch (IOException e) {
+            LOGGER.error("Redirect failure", e);
+        }
+    }
+
+
+    /**
+     * 将属性放入session
+     */
+    public static void setSessionAttribute(String key, Object value) {
+        getSession().setAttribute(key, value);
+    }
+
+    /**
+     * 从session中获取
+     */
+    public static <T> T getSessionAttribute(String key) {
+        return (T) getRequest().getSession().getAttribute(key);
+    }
+
+    /**
+     * 从session中移除属性
+     */
+    public static void removeSessionAttribute(String key) {
+        getRequest().getSession().removeAttribute(key);
+    }
+
+    /**
+     * session失效
+     */
+    public static void invalidateSession() {
+        getRequest().getSession().invalidate();
+    }
+
 
 }
